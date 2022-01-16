11858878d96a0c675bdf41ae532cebf8b0760568
Myles <bracken.myles@gmail.com>
2022-01-16 11:36:58 -0800

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Toggle() {
-  const [isOn, setIsOn] = useState(true);
+  const [isOn, setIsOn] = useState(false);
 
   function handleClick() {
     setIsOn((isOn) => !isOn)
